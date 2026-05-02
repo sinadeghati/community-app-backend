b33052910c0d79b14967ba2cb2a3d08d4aa2e087
sinadeghati <cdeghati@gmail.com>
2026-05-02 13:15:45 -0700

    make price optional

--- iranapp/listings/models.py
+++ iranapp/listings/models.py
@@ -14,7 +14,7 @@ class Listing(models.Model):
     title = models.CharField(max_length=255)
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
     contact_info = models.CharField(max_length=255)
     category = models.CharField(max_length=50, blank=True, default="")
