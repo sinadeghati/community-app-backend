783d0b9602bf256d73e551df8cadd410d39b0902
sinadeghati <cdeghati@gmail.com>
2026-06-17 10:00:14 -0700

    Add SimpleJWT token refresh endpoint at /api/token/refresh/

--- iranapp/iranapp/urls.py
+++ iranapp/iranapp/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -14,7 +15,11 @@ urlpatterns = [
 
     path('api/accounts/', include('accounts.urls')),
 
-    
+    path(
+        "api/token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh",
+    ),
 
 ]
 
